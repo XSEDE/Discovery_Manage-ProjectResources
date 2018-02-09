7246526a18a1e499d6634474c1f8a84e63491aef
navarro <navarro@1370f987-b873-4e1f-8c66-8b3e5403fd71>
2018-02-09 12:52:38 +0000

    Fix to point to renamed config

--- xci/source/info.apps/Manage-ProjectResources/trunk/sbin/route_projectresources.sh
+++ xci/source/info.apps/Manage-ProjectResources/trunk/sbin/route_projectresources.sh
@@ -5,7 +5,7 @@ PYTHON=/soft/python-current/bin/python
 export LD_LIBRARY_PATH=/soft/python-current/lib/
 
 export PYTHONPATH=${WAREHOUSE_ROOT}/django_xsede_warehouse
-export DJANGO_CONF=/soft/warehouse-1.0/conf/settings_info_mgmt.conf
+export DJANGO_CONF=/soft/warehouse-1.0/conf/django_prod_mgmt.conf
 export DJANGO_SETTINGS_MODULE=xsede_warehouse.settings
 
 ${PYTHON} ${MY_ROOT}/sbin/route_projectresources.py ${@:1}
