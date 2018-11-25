e5efdc0db0768d4c90019cfae281062428719f99
JP Navarro <jpfnavarro@gmail.com>
2018-11-25 13:38:57 +0000

    Fix PYTHON_ROOT and DJANGO_CONF

--- sbin/route_projectresources.sh
+++ sbin/route_projectresources.sh
@@ -5,11 +5,11 @@ WAREHOUSE_ROOT=/soft/warehouse-1.0/PROD
 PYTHON_BASE=/soft/python/python-3.6.6-base
 export LD_LIBRARY_PATH=${PYTHON_BASE}/lib
 
-PYTHON_ROOT=/soft/warehouse-apps-1.0/python
+PYTHON_ROOT=/soft/warehouse-apps-1.0/Manage-ProjectResources/python
 source ${PYTHON_ROOT}/bin/activate
 
 export PYTHONPATH=${WAREHOUSE_ROOT}/django_xsede_warehouse
-export DJANGO_CONF=/soft/warehouse-1.0/conf/django_prod_mgmt.conf
+export DJANGO_CONF=/soft/warehouse-apps-1.0/Manage-ProjectResources/conf/django_xsede_warehouse.conf
 export DJANGO_SETTINGS_MODULE=xsede_warehouse.settings
 
 python ${MY_ROOT}/sbin/route_projectresources.py ${@:1}
