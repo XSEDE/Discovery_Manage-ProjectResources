73d66608707874f51fa8b5d647b63c9c3339453a
navarro <navarro@1370f987-b873-4e1f-8c66-8b3e5403fd71>
2019-11-12 19:41:34 +0000

    Convert to Python 3.7.5

--- xci/source/info.apps/Manage-ProjectResources/trunk/sbin/route_projectresources.sh
+++ xci/source/info.apps/Manage-ProjectResources/trunk/sbin/route_projectresources.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-MY_ROOT=/soft/warehouse-apps-1.0/Manage-ProjectResources/PROD
-WAREHOUSE_ROOT=/soft/warehouse-1.0/PROD
+APP_BASE=/soft/warehouse-apps-1.0/Manage-ProjectResources
+APP_SOURCE={APP_BASE}/PROD
+WAREHOUSE_SOURCE=/soft/warehouse-1.0/PROD
 
-PYTHON_BASE=/soft/python/python-3.6.6-base
+PYTHON_BASE=${APP_BASE}/`cat python/lib/python*/orig-prefix.txt`
 export LD_LIBRARY_PATH=${PYTHON_BASE}/lib
 
-PYTHON_ROOT=/soft/warehouse-apps-1.0/Manage-ProjectResources/python
-source ${PYTHON_ROOT}/bin/activate
+PIPENV_BASE=${APP_BASE}/python
+source ${PIPENV_BASE}/bin/activate
 
-export PYTHONPATH=${WAREHOUSE_ROOT}/django_xsede_warehouse
-export DJANGO_CONF=/soft/warehouse-apps-1.0/Manage-ProjectResources/conf/django_xsede_warehouse.conf
+export PYTHONPATH=${WAREHOUSE_SOURCE}/django_xsede_warehouse
+export DJANGO_CONF=${APP_BASE}/conf/django_xsede_warehouse.conf
 export DJANGO_SETTINGS_MODULE=xsede_warehouse.settings
 
-python ${MY_ROOT}/sbin/route_projectresources.py ${@:1}
+${PIPENV_BASE}/bin/python ${APP_SOURCE}/sbin/route_projectresources.py ${@:1}
